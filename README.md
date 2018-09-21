8b108f401a409f69b7272ea87231e6fd98a9478e
TryOSdev-Steven <steven35953@gmail.com>
2018-09-20 21:12:44 -0400

    finished implementing simulated annealing, needs to be properly tested and the temperature function should be replaced with a better more complex function for cooling schedule

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SimulatedAnnealing
-a implementation of the simulated annealing algorithm
+a implementation of the simulated annealing algorithm as an example for finding solutions to the traveling salesman problem
--- src/Annealing.cpp
+++ src/Annealing.cpp
@@ -6,23 +6,25 @@ void gen_path(std::vector<unsigned int>& output, unsigned int mapSize);
 float fitness(const std::vector<std::pair<int, int>>& map, const std::vector<unsigned int>& path);
 float dist(const std::pair<int, int>* from, const std::pair<int, int>* to);
 void step(const std::vector<unsigned int>& src, std::vector<unsigned int>& dest);
+float temperature(float currTime);
+bool P(float s, float snew, float t);
 
 void Anneal(const std::vector<std::pair<int, int>>& map, int max_iters)
 {
 	/*
-	Simmulated aNnealing on TSP:
+	Simmulated Annealing on TSP:
 		Givens:
 			- Graph Layout
 
 		Step 1:
 			- stocastically pick a path to take (*DONE*)
 		Step 2:
-			- get the fitness of that path
+			- get the fitness of that path (*DONE*)
 		Step 3: 
-			- for i < MAX_ITERS
-				- Move to a neighboring state.
-				- get the new states fitness
-				- determine wether to move to the new state with some tempurature function
+			- for i < MAX_ITERS (*DONE*)
+				- Move to a neighboring state. (*DONE*)
+				- get the new states fitness (*DONE*)
+				- determine wether to move to the new state with some tempurature function (*DONE*)
 
 	*/
 	srand((unsigned) time(NULL));
@@ -32,20 +34,10 @@ void Anneal(const std::vector<std::pair<int, int>>& map, int max_iters)
 	auto newPath = std::vector<unsigned int>();
 	gen_path(path, map.size());
 
-	float fit = fitness(map, path);
-	float newFit = 1234.5f;
 	for (int i = 0; i < max_iters; ++i) {
 		step(path, newPath);
-		if ((newFit = fitness(map, newPath)) < fit) {
-			//newPath is a better path so take it
-			fit = newFit;
-			path.swap(newPath); //swap to save copies
-
-		} else {
-			//newPath is a worse path, use temperature to decide wether to take it
-			
-
-
+		if (P(fitness(map, path), fitness(map, newPath), temperature(i / max_iters)) >= ((float)rand() / (float)RAND_MAX)) {
+			path.swap(newPath); //swap to avoid copies
 		}
 	}
 	
@@ -77,6 +69,8 @@ float fitness(const std::vector<std::pair<int, int>>& map, const std::vector<uns
 		lastPoint = &map[path[i]];
 	}
 
+	sum += dist(lastPoint, &map[path[0]]); //dont forget to go back to the start
+
 	return sum;
 }
 
@@ -94,4 +88,13 @@ void step(const std::vector<unsigned int>& src, std::vector<unsigned int>& dest)
 
 	std::reverse(dest.begin() + _1st, dest.begin() + _2nd); //reverse the subsection of the dest vector to take the "step" 
 
+}
+
+float temperature(float currTime) {
+	return (1 / currTime);
+}
+
+bool P(float s, float snew, float t) 
+{
+	return (snew < s) ? 1 : exp(-(snew - s)/t);
 }
